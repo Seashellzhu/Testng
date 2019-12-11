99c686d027f7182f536aed10f38f26524177dfcf
Seashellzhu <58252001@qq.com>
2019-12-11 14:22:39 +0800

    errrrr

--- src/testproject/Testproject2.java
+++
@@ -1,9 +0,0 @@
-package testproject;
-
-import com.sun.xml.internal.ws.addressing.model.MissingAddressingHeaderException;
-
-public class Testproject2 {
-public static void main(String[] args) {
-	System.out.println("test2");
-}
-}
